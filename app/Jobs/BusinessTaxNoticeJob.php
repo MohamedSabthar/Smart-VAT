3b4a0efca40b7461d36f2016b058946ac36b1e12
Mohamed Sabthar <sabtharugc0@gmail.com>
2019-10-15 14:55:12 +0530

    added queue failed() methode to BusinessTaxNoticeJob

--- app/Jobs/BusinessTaxNoticeJob.php
+++ app/Jobs/BusinessTaxNoticeJob.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use Exception;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
@@ -32,8 +33,20 @@ class BusinessTaxNoticeJob implements ShouldQueue
      */
     public function handle()
     {
-        $mail = $this->id.'@yopmail.com';
+        $mail =  $this->id!=2 ?$this->id.'@yopmail.com' : $this->id;
         // dd($mail);
-        Mail::to($this->id.'@yopmail.com')->send(new BusinessTaxNotice); // Sending mail to Queue process
+        echo($mail);
+        Mail::to($mail)->send(new BusinessTaxNotice); // Sending mail to Queue process
+    }
+
+    /**
+     * The job failed to process.
+     *
+     * @param  Exception  $exception
+     * @return void
+     */
+    public function failed(Exception $exception)
+    {
+        Mail::to('sabtharugc0@gmail.com')->send(new BusinessTaxNotice);
     }
 }
