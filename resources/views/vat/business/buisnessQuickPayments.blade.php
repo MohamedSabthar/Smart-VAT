a2acc1e5379c8aa908180bd661f7a0c77226d590
Mohamed Sabthar <sabtharugc0@gmail.com>
2019-11-13 13:05:42 +0530

    business Tax quick payment implementation

---
+++ resources/views/vat/business/buisnessQuickPayments.blade.php
@@ -0,0 +1,151 @@
+@extends('layouts.app')
+
+@section('title','Business Payment')
+
+@push('css')
+<link rel="stylesheet" href="{{asset('assets/css/dataTables.bootstrap4.min.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/custom-data-table.css')}}">
+@endpush
+
+@section('sidebar')
+@includeWhen(Auth::user()->role=='admin','admin.include.sidebar')
+@includeWhen(Auth::user()->role=='employee','employee.include.sidebar')
+@endsection
+
+@section('header')
+<div class="col-xl-3 col-lg-6">
+    <div class="card card-stats mb-4 mb-xl-0">
+        <div class="card-body">
+            <div class="row">
+                <div class="col">
+                    <h5 class="card-title text-uppercase text-muted mb-0">Payment List</h5>
+                    <span class=" font-weight-bold mb-0">924</span>
+                </div>
+                <div class="col-auto">
+                    <div class="icon icon-shape bg-yellow text-white rounded-circle shadow">
+                        <i class="fas fa-users"></i>
+                    </div>
+                </div>
+            </div>
+            <p class="mt-3 mb-0 text-muted text-sm">
+                <span class="text-warning mr-2"><i class="fas fa-arrow-down"></i> 1.10%</span>
+                <span class="text-nowrap">Since yesterday</span>
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="col-xl-3 col-lg-6">
+    <div class="card card-stats mb-4 mb-xl-0">
+        <div class="card-body">
+            <div class="row">
+                <div class="col">
+                    <h5 class="card-title text-uppercase text-muted mb-0">New users</h5>
+                    <span class="h2 font-weight-bold mb-0">2,356</span>
+                </div>
+                <div class="col-auto">
+                    <div class="icon icon-shape bg-warning text-white rounded-circle shadow">
+                        <i class="fas fa-chart-pie"></i>
+                    </div>
+                </div>
+            </div>
+            <p class="mt-3 mb-0 text-muted text-sm">
+                <span class="text-danger mr-2"><i class="fas fa-arrow-down"></i> 3.48%</span>
+                <span class="text-nowrap">Since last week</span>
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="col-xl-3 col-lg-6">
+    <div class="card card-stats mb-4 mb-xl-0">
+        <div class="card-body">
+            <div class="row">
+                <div class="col">
+                    <h5 class="card-title text-uppercase text-muted mb-0">New users</h5>
+                    <span class="h2 font-weight-bold mb-0">2,356</span>
+                </div>
+                <div class="col-auto">
+                    <div class="icon icon-shape bg-warning text-white rounded-circle shadow">
+                        <i class="fas fa-chart-pie"></i>
+                    </div>
+                </div>
+            </div>
+            <p class="mt-3 mb-0 text-muted text-sm">
+                <span class="text-danger mr-2"><i class="fas fa-arrow-down"></i> 3.48%</span>
+                <span class="text-nowrap">Since last week</span>
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="col-xl-3 col-lg-6">
+    <div class="card card-stats mb-4 mb-xl-0">
+        <div class="card-body">
+            <div class="row">
+                <div class="col">
+                    <h5 class="card-title text-uppercase text-muted mb-0">New users</h5>
+                    <span class="h2 font-weight-bold mb-0">2,356</span>
+                </div>
+                <div class="col-auto">
+                    <div class="icon icon-shape bg-warning text-white rounded-circle shadow">
+                        <i class="fas fa-chart-pie"></i>
+                    </div>
+                </div>
+            </div>
+            <p class="mt-3 mb-0 text-muted text-sm">
+                <span class="text-danger mr-2"><i class="fas fa-arrow-down"></i> 3.48%</span>
+                <span class="text-nowrap">Since last week</span>
+            </p>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+@section('pageContent')
+
+<div class="row">
+    <div class="col">
+        <div class="card bg-secondary shadow">
+            <div class="card-header bg-white border-0">
+                <h3 class="mb-0"><span class="text-uppercase">Accept payments</span></h3>
+            </div>
+
+            <div class="card-body">
+                {{-- Employee registration form --}}
+                <form method="POST" action="{{route('register')}}">
+                    @csrf
+                    <div class="form-group row pt-3">
+                        <label for="example-week-input" class="col-md-2 col-form-label form-control-label">NIC</label>
+                        <div class="col-md-10">
+                            <input class="form-control @error('nic') is-invalid @enderror" type="text"
+                                value="{{old('nic')}}" id="nic" name="nic" placeholder="Enter vat payer's NIC">
+                            @error('nic')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                    </div>
+
+
+                </form>
+                {{-- end of Employee registration form --}}
+            </div>
+
+        </div>
+    </div>
+</div>
+@endsection
+
+@push('script')
+<script src="{{asset('js/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('js/dataTables.bootstrap4.min.js')}}"></script>
+<script>
+    $(document).ready(function() {
+
+        
+      });
+</script>
+@endpush
--- routes/web.php
+++ routes/web.php
@@ -66,9 +66,12 @@ Route::post('/business/business-register/{id}', 'vat\BusinessTaxController@regis
 Route::get('/business/payments/{shop_id}', 'vat\BusinessTaxController@businessPayments')->name('business-payments');
 Route::post('/business/payments/{shop_id}', 'vat\BusinessTaxController@reciveBusinessPayments')->name('receive-business-payments');
 Route::get('/business/business-remove/{shop_id}', 'vat\BusinessTaxController@removeBusiness')->name('remove-business'); // soft delete business route
-Route::get('/business/payment-remove/{id}','vat\BusinessTaxController@removePayment')->name('remove-payment');//soft delete business payment
-Route::get('/business/payment-restore/{shop_id}','vat\BusinessTaxController@restorePayment')->name('restore-payment');//restore payment
+Route::get('/business/payment-remove/{id}', 'vat\BusinessTaxController@removePayment')->name('remove-payment');//soft delete business payment
+Route::get('/business/payment-restore/{shop_id}', 'vat\BusinessTaxController@restorePayment')->name('restore-payment');//restore payment
 Route::post('/business/get-business-types', 'vat\BusinessTaxController@getBusinestypes')->name('get-business-types');
+Route::get('/business/quick-payments', function () {
+    return view('vat.business.buisnessQuickPayments');
+});
 //all business tax related tax routes should starts with "/buisness"
 
 
